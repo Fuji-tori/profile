70b24a7eb7fdaceacc53293a8b373c603273f390
Fuji-tori <out.fuji96aya@gmail.com>
2022-07-18 23:54:02 +0900

    fix

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
 
 module Profile
